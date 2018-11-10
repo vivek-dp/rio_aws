8e19dd423dcb20a28374e5f56063e7c3c1aa0dd7
vivek-dp <vivek@decorpot.com>
2018-11-10 09:36:53 +0530

    Save model to aws added. Now for folder3/.

--- controller/save_model.rb
+++ controller/save_model.rb
@@ -1,29 +1,40 @@
 module RioModel
+	AWS_ASSETS_REGION ||= 'ap-south-1'
 	def self.addObservers
 		Sketchup.active_model.add_observer(RioModelObserver.new)
 	end
 
-  class RioModelObserver < Sketchup::ModelObserver
-    def onSaveModel(model)
-      puts "onSaveModel" + model.to_s
-      Example.exportModel()
-    end
-  end
+	class RioModelObserver < Sketchup::ModelObserver
+		def onSaveModel(model)
+			puts "onSaveModel" + model.to_s
+			RioModel.exportModel()
+		end
+	end
 
-  def self.exportModel()
-    return if @exporting
+	def self.exportModel()
+		return if @exporting
 
-    puts "exportModel()"
-    model = Sketchup.active_model
+		puts "exportModel()"
+		model 		= Sketchup.active_model
+		s3_client 	= RioAwsDownload::get_client
+		
+		file_path 	= DP::current_file_path
+		target_path = "folder3/"+File.basename(file_path) #Change to user_name
+		puts Time.now
+		
+		resp = s3_client.put_object({
+			body: file_path, 
+			bucket: "test.rio.assets", 
+			key: target_path, 
+		})	
 
-    @exporting = true
-	puts "export : #{@exporting} " 
-  ensure
-	puts "ennsure : #{@exporting}"
-    # Just in case model.export should raise an exception we guarranty the flag
-    # is reset.
-    @exporting = false
-  end
+		puts Time.now if resp
+		puts "resp : #{resp}"
+
+		@exporting = true
+	ensure
+		@exporting = false
+	end
 
 end
 
